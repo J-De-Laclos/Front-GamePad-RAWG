47bd0902d4588ff53b74633c5531049826224b94
Fruteau Jonathan <Jonathan@air-de-pauline.home>
2023-12-27 10:52:07 +0400

    fixed bug

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // C'est le dossier de build que tu souhaites
+    outDir: "dist",
   },
 });
